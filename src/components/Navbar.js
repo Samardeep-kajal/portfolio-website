2561e0d4e78e47aae24293cbaeb9ade6007aa8a6
Samardeep Kajal <samk712003@gmail.com>
2024-02-12 19:05:25 +0530

    Added navbar response to scroll handler.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "./Logo";
 
 const Navbar = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+      setIsHidden(scrollTop > lastScrollTop && scrollTop > 100);
+      setLastScrollTop(scrollTop);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollTop]);
   return (
-    <div className="bg-[#1b263b] flex justify-between items-center h-20 max-w-h mx-auto px-4 text-white">
+    <div
+      className={`bg-[#1b263b] flex justify-between items-center h-20 max-w-h mx-auto px-4 text-white sticky top-0 transition-transform ${
+        isHidden ? "-translate-y-full" : ""
+      }`}
+    >
       {/* Logo */}
       <Logo />
       {/* Desktop Navigation */}
