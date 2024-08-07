eabd20bb22fd744a00ffa4f8c5a1a07366610fee
Samardeep-kajal <samk712003@gmail.com>
2024-08-07 21:47:37 +0530

    Added photo in portfolio, modified sections and style changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import samardeepImage from "./media/samardeep.PNG";
 
 function App() {
   const helloArray = ["Olá", "Hello", "Bonjour", "नमस्ते"];
@@ -22,21 +23,63 @@ function App() {
       <div className="bg-[#E0E1DD] min-h-screen">
         {/* Background color applied to this wrapper */}
         <Navbar />
-        <div className="mt-32 ml-56 w-3/4">
-          <p className="text-[#4171b0] text-2xl ">
-            {helloArray[count]}, my name is
-          </p>
-          <h1 className="mt-4 text-6xl font-bold ">Samardeep Kajal.</h1>
-          <p className=" text-[#d97d77] mt-5 text-4xl">
-            I create full stack web applications.
-          </p>
-          <p className="mt-5 text-lg w-2/4" style={{ textAlign: "justify" }}>
-            I am currently pursuing my Bachelor's degree in Information
-            Technology and am set to graduate in 2024. Throughout my academic
-            journey, I have undertaken various full-stack web development
-            projects, demonstrating my passion and dedication for software
-            development.
-          </p>
+        {/* <div className="mt-32 ml-28 grid grid-cols-2 w-full">
+          <div className=" w-3/4 col-span-1">
+            <p className="text-[#4171b0] text-2xl ">
+              {helloArray[count]}, my name is
+            </p>
+            <h1 className="mt-4 text-6xl font-bold ">Samardeep Kajal.</h1>
+            <p className=" text-[#d97d77] mt-5 text-4xl">
+              I create full stack web applications.
+            </p>
+            <p className="mt-5 text-lg w-2/4" style={{ textAlign: "justify" }}>
+              I am currently pursuing my Bachelor's degree in Information
+              Technology and am set to graduate in 2024. Throughout my academic
+              journey, I have undertaken various full-stack web development
+              projects, demonstrating my passion and dedication for software
+              development.
+            </p>
+          </div>
+          <div className=" w-1/4 col-span-1">
+            <div className="w-1/4 ml-10">
+              <img
+                src={samardeepImage}
+                alt="Samardeep Kajal"
+                className="rounded-full"
+              />
+            </div>
+          </div>
+        </div> */}
+        <div className="mt-12 flex justify-center">
+          <div className="flex flex-row items-center w-3/4">
+            <div className="w-3/5">
+              <p className="text-[#4171b0] text-2xl">
+                {helloArray[count]}, my name is
+              </p>
+              <h1 className="mt-4 text-6xl font-bold">Samardeep Kajal.</h1>
+              <p className="text-[#d97d77] mt-5 text-4xl">
+                I create full stack web applications.
+              </p>
+              <p className="mt-5 text-lg" style={{ textAlign: "justify" }}>
+                I am currently pursuing my Bachelor's degree in Information
+                Technology and am set to graduate in 2024. Throughout my
+                academic journey, I have undertaken various full-stack web
+                development projects, demonstrating my passion and dedication
+                for software development.
+              </p>
+            </div>
+            <div className="w-2/5 flex justify-center ml-24">
+              <img
+                src={samardeepImage}
+                alt="Samardeep Kajal"
+                className="border-4 border-solid border-[#d97d77] rounded-full h-5/6 w-5/6 object-cover"
+                onContextMenu={(e) => e.preventDefault()}
+                draggable="false"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-32 ml-36 w-3/4">
           <Skills />
           <Projects className="ml-5" />
           {/* Footer */}
