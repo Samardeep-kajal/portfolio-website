e3008c60cb4f990708cf96347aac8483425e3bfb
Samardeep Kajal <samk712003@gmail.com>
2024-12-25 23:51:36 +0530

    Fixing styling for deployed prototype

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,7 +16,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Projects = () => {
   return (
-    <div className="md:mt-24 md:ml-52 md:w-4/6">
+    <div className="md:mt-24 md:ml-52 md:w-4/6 z-10">
       <h1 className="text-3xl ml-36 md:ml-0 underline font-Poppins mb-8">
         Projects
       </h1>
@@ -43,7 +43,7 @@ const Projects = () => {
             </div>
           </Carousel>
           <div className="mt-4">
-            <p className="text-base mb-4" style={{ textAlign: "justify" }}>
+            <p className="text-base mb-4 text-justify md:text-left">
               Savvy Slice is an innovative online pizza ordering platform
               developed using the MERN (MongoDB, Express.js, React.js, Node.js)
               stack. This dynamic web application allows users to browse through
@@ -66,11 +66,11 @@ const Projects = () => {
               <span className="bg-[#415A77] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
                 MongoDB
               </span>
-              <span className="bg-[#415A77] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+              <span className="bg-[#415A77] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2 ">
                 MaterialUI
               </span>
             </div>
-            <div className="flex flex flex-wrap">
+            <div className="flex flex-wrap">
               <button className="border-black text border-2 px-3 mr-2 h-14 font-semibold hover:bg-[#778DA9] rounded-md flex items-center transition-colors duration-300 ease-in-out">
                 Check Github{" "}
                 <img
@@ -108,10 +108,7 @@ const Projects = () => {
               </div>
             </Carousel>
             <div className="mt-4">
-              <p
-                className="text-base mb-4 text-justify md:text-left"
-                style={{ textAlign: "justify" }}
-              >
+              <p className="text-base mb-4 text-justify md:text-left">
                 The Self Journaling Platform is a transformative web application
                 designed to empower users on their journey of self-discovery and
                 personal growth. With intuitive journaling features, robust data
@@ -138,7 +135,7 @@ const Projects = () => {
                   react-redux
                 </span>
               </div>
-              <div className="flex flex flex-wrap">
+              <div className="flex flex-wrap">
                 <button className="border-black text border-2 px-3 mr-2 h-14 font-semibold hover:bg-[#778DA9] rounded-md flex items-center transition-colors duration-300 ease-in-out">
                   Check Github{" "}
                   <img
